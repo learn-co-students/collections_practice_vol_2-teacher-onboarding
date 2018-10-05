355ce26d63da942be24613493fed5df72e69a93a
Joseph Fraiser <jfrasier@SF-US04880-MBP13.local>
2018-10-05 09:55:41 -0500

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,26 @@
-# your code goes here
+def begins_with_r (array)
+    condition_holder = false
+    array.each do |word|
+        if word.slice(0) != "r"
+            condition_holder = false
+        elsif word.slice(0) == "r"
+            condition_holder = true 
+        end 
+    end
+    return condition_holder
+end
+
+def contain_a (array)
+    return array.grep /a/
+end
+
+def first_wa (array)
+    result = nil
+    array.each do |word|
+        if word.slice(0) == "w" && word.slice(1) == "a"
+            result = word
+            break
+        end
+    end
+    return result
+end
