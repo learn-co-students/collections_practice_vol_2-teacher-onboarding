3eb41910df381dc76f393a3d8c79bcbc9a19e072
Levi Miller <paullevimiller@gmail.com>
2019-07-18 08:47:17 -0700

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -66,21 +66,57 @@ def count_elements(array)
 end
 
 # test_hash_array = [{:name => "blake"}, {:name => "blake"}, {:name => "ashley"}]
-
 # count_elements(test_hash_array)
 
 def merge_data(keys, data)
     merged_data = keys
-    keys.each do |hash|
-        data.each do |name|
-            binding.pry
-            if name == hash[:first_name]
-                name.each do |key, value|
-                    merged_data[key] = value
-                    
+    merged_data.each do |hash|
+        data[0].each do |name, traits|
+            if hash[:first_name] == name
+                traits.each do |trait, value|
+                    hash[trait] = value
                 end
             end
         end
     end
     return merged_data
 end
+
+def find_cool(hash_array)
+    # binding.pry
+    cool_hash_array = []
+    hash_array.each do |hash|
+        if hash[:temperature] == "cool"
+            # binding.pry
+            cool_hash_array << hash
+        end
+    end
+    return cool_hash_array
+end
+
+def organize_schools(schools)
+    school_location_hash = {}
+    schools.each do |school, location_hash|
+        if school_location_hash.include?(location_hash[:location]) == false
+            school_location_hash[location_hash[:location]] = [school]
+        else
+            school_location_hash[location_hash[:location]] << school
+        end
+    end
+    return school_location_hash
+end
+
+test_schools = {
+    "flatiron school bk"=>{:location=>"NYC"},
+    "flatiron school"=>{:location=>"NYC"},
+    "dev boot camp"=>{:location=>"SF"},
+    "dev boot camp chicago"=>{:location=>"Chicago"},
+    "general assembly"=>{:location=>"NYC"},
+    "Hack Reactor"=>{:location=>"SF"}
+}
+
+expected_output = {
+    "Chicago"=>["dev boot camp chicago"], 
+    "NYC"=>["flatiron school bk", "flatiron school", "general assembly"], 
+    "SF"=>["dev boot camp", "Hack Reactor"]
+}
